991250f230c550fb4381d2545189e9e830fe487a
Gabriel Goldas <gabriel.goldas@gmail.com>
2025-04-24 08:25:37 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img src="./src/assets/github-white.png" alt="Logo GitHub" />
+      <h1>Perfil <b>GitHub</b></h1>
+    </>
+  )
+}
+
+export default App
